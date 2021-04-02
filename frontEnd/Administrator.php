ebcb0b641feddfc702fe3588a23b7cb727ddd0fd
DeliarM <lioneldeliar520@gmail.com>
2021-04-02 17:51:03 -0600

    update view

--- frontEnd/Administrator.php
+++ frontEnd/Administrator.php
@@ -1,25 +1,59 @@
-<!DOCTYPE html>
-<!--References: https://www.youtube.com/watch?v=v1PeTDrw6OY-->
-<html>
-    <head>
-        <meta charset= "US-ASCII">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel = "stylesheet" href = "Administrator.css">
-        <script defer src = "https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-        <Title>
-            Admin Page
-        </Title>
-    </head>
-    <body>
-    <h1> Administrator</h1>
+<?php
+    session_start();
+?>
 
-<!--Buttons-->
-<div class="button-container">
-    <button class = "Add-Course" id="Add-Course">Add Course</button>
-    <button class = "Remove-Course" id="Remove-Course">Remove Course</button>
-    <button class = "Add-New-Student" id="Add-New-Student">Add New Student</button>
-    <button class = "Remove-Student" id="Remove-Student">Remove Student</button>
-</div>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset= "US-ASCII">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <Title>Admin Page</Title>
+    <link rel="stylesheet" href="admin.css">
+</head>
+    
+<body>
+
+<header>
+    <nav>
+        <h1> Hello <?php echo $_SESSION['Fname']; ?> <?php echo $_SESSION['Lname'];?> </h1>
+        <ul class="tabs">
+            <li class="tab is-active">
+                <a data-switcher data-tab="1"> Add Course </a>
+            </li>
+            <li class="tab">
+                <a data-switcher data-tab="2"> Remove Course </a>
+            </li>
+            <li class="tab">
+                <a data-switcher data-tab="3"> Add Student </a>
+            </li>
+            <li class="tab">
+                <a data-switcher data-tab="4"> Remove Student </a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <section class="pages"> 
+    <div class="page is-active" data-page="1">
+        <h2>Page 1</h2>
+        <p>Welcome to the Add course page</p>
+    </div>
+    <div class="page" data-page="2">
+        <h2>Page 2</h2>
+        <p>Welcome to the remove Course page</p>
+    </div>
+    <div class="page" data-page="3">
+        <h2>Page 3</h2>
+        <p>Welcome to the add student page</p>
+    </div>
+    <div class="page" data-page="4">
+        <h2>Page 4</h2>
+        <p>Welcome to the remove student page</p>
+    </div>
+    </section>
+</main>
+
+<script src="admin.js"></script>
+</body>
 </html>
--- frontEnd/Student.php
+++ frontEnd/Student.php
@@ -2,33 +2,52 @@
     session_start();
 ?>
 
-<html>
-    <head>
-        <meta charset= "US-ASCII">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel = "stylesheet" href = "student.css">
-        <script defer src = "https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-        <Title>
-            Schedule Page
-        </Title>
-    </head>
-    <body>
-        <h1> Student <?php echo $_SESSION['Fname']; ?> <?php echo $_SESSION['Lname']; ?></h1>
-<!--Searc Button-->
-<div class = "Search-Courses-Box" >
-    <input class = "Search-Text" type="text" name = "" placeholder = "Type in Your Course!" >
-    <a class = "search-button" href = "#">
-    <i class = "fas fa-search"></i>
-    </a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset= "US-ASCII">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <Title>Student Page</Title>
+    <link rel="stylesheet" href="admin.css">
+</head>
+    
+<body>
 
-</div>
+<header>
+    <nav>
+        <h1> Hello <?php echo $_SESSION['Fname']; ?> <?php echo $_SESSION['Lname'];?> </h1>
+        
+        <ul class="tabs">
+            <li class="tab is-active">
+                <a data-switcher data-tab="1"> Enroll in a Course </a>
+            </li>
+            <li class="tab">
+                <a data-switcher data-tab="2"> Drop a Course </a>
+            </li>
+            <li class="tab">
+                <a data-switcher data-tab="3"> View Courses </a>
+            </li>
+        </ul>
+    </nav>
+</header>
 
-<div class="button-container">
-    <button class = "View-Current-Courses" id="View-Current-Courses">View Current Courses</button>
-    <button class = "Select-Courses" id="Select-Courses">Select Courses</button>
-</div>
+<main>
+    <section class="pages"> 
+    <div class="page is-active" data-page="1">
+        <h2>Page 1</h2>
+        <p>Welcome to the enroll page</p>
+    </div>
+    <div class="page" data-page="2">
+        <h2>Page 2</h2>
+        <p>Welcome to the drop Course page</p>
+    </div>
+    <div class="page" data-page="3">
+        <h2>Page 3</h2>
+        <p>Welcome to the view page</p>
+    </div>
+    </section>
+</main>
 
-
-    </body>
+<script src="admin.js"></script>
+</body>
 </html>
